53ccea961098262f04d5628b2a361b42bd8bc695
williamloree <mreye.gaming@hotmail.com>
2024-03-03 21:09:57 +0100

    test action

--- src/tests/user.test.ts
+++ src/tests/user.test.ts
@@ -1,40 +1,40 @@
-import { ApolloServer } from "@apollo/server";
-import assert from "assert";
+// import { ApolloServer } from "@apollo/server";
+// import assert from "assert";
 
-import { typeDefs } from "../typeDefs";
-import { resolvers } from "../resolvers";
-import { Context } from "../@types/graphql";
-import AppDataSource from "../config/database";
+// import { typeDefs } from "../typeDefs";
+// import { resolvers } from "../resolvers";
+// import { Context } from "../@types/graphql";
+// import AppDataSource from "../config/database";
 
-import { CreateUser } from "./gql/User";
+// import { CreateUser } from "./gql/User";
 
-let testServer: ApolloServer<Context>;
+// let testServer: ApolloServer<Context>;
 
-const user = {
-  email: "williamloree@hotmail.fr",
-  password: "qwertyuiop",
-};
+// const user = {
+//   email: "williamloree@hotmail.fr",
+//   password: "qwertyuiop",
+// };
 
-beforeAll(async () => {
-  testServer = new ApolloServer({
-    typeDefs,
-    resolvers,
-  });
-});
-
-// afterAll(async () => {
-//   await testServer?.stop();
+// beforeAll(async () => {
+//   testServer = new ApolloServer({
+//     typeDefs,
+//     resolvers,
+//   });
 // });
 
-describe("Test User", () => {
-  it("createUser", async () => {
-    AppDataSource.initialize();
-    const response = await testServer.executeOperation({
-      query: CreateUser,
-      variables: { input: user },
-    });
-    assert(response.body.kind === "single");
-    expect(response.body.singleResult.errors).toBeUndefined();
-    expect(response.body.singleResult.data?.createUser).toBe(user);
-  });
-});
+// // afterAll(async () => {
+// //   await testServer?.stop();
+// // });
+
+// describe("Test User", () => {
+//   it("createUser", async () => {
+//     AppDataSource.initialize();
+//     const response = await testServer.executeOperation({
+//       query: CreateUser,
+//       variables: { input: user },
+//     });
+//     assert(response.body.kind === "single");
+//     expect(response.body.singleResult.errors).toBeUndefined();
+//     expect(response.body.singleResult.data?.createUser).toBe(user);
+//   });
+// });
